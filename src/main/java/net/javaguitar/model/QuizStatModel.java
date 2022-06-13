3a20a658205e8fe7ea138f0959578bd0a1b6567c
linz08 <javaguitar@gmail.com>
2022-06-13 23:56:29 +0900

    퀴즈통계 시작

---
+++ src/main/java/net/javaguitar/model/QuizStatModel.java
@@ -0,0 +1,18 @@
+package net.javaguitar.model;
+
+import lombok.Data;
+
+@Data
+public class QuizStatModel {
+
+    public QuizStatModel() {
+        // TODO Auto-generated constructor stub
+    }
+
+    private String quiz_date;
+    private int doc_code;
+    private int quiz_number;
+    private int quiz_num;
+    private String success_yn;
+    private int quiz_time;
+}
