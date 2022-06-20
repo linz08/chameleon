f679fd24d4df9ddb3e90fecaeaa44f27b4609777
linz08 <javaguitar@gmail.com>
2022-06-20 23:30:12 +0900

    퀴즈 북마크 추가(진행중)

---
+++ src/main/java/net/javaguitar/model/QuizBMModel.java
@@ -0,0 +1,15 @@
+package net.javaguitar.model;
+
+import lombok.Data;
+
+@Data
+public class QuizBMModel {
+
+    public QuizBMModel() {
+        // TODO Auto-generated constructor stub
+    }
+
+    private int doc_code;
+    private int quiz_number;
+    private String create_date;
+}
